4732c93ce3f12801f7e2885b3cc15cec709805a1
fahmid prachaw <fahmidprachaw@outlook.com>
2024-03-08 23:32:13 +0600

    complete

--- products/views.py
+++ products/views.py
@@ -93,7 +93,7 @@ class UserRegistrationViewSet(viewsets.ViewSet):
             print("token ", token)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             print("uid ", uid)
-            confirm_link = f"http://127.0.0.1:8000/products/active/{uid}/{token}"
+            confirm_link = f"https://cloth-store-backend.onrender.com/products/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             
